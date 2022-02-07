5003b3d9b52706311a97521d49e6ad592e8a93ba
Ruojing Peng <rppeng@pauling.caltech.edu>
2022-02-07 14:28:18 -0800

    switching to develop

--- quimb/tensor/fermion/global_grad.py
+++ quimb/tensor/fermion/global_grad.py
@@ -1,15 +1,20 @@
 import numpy as np
+import time
+
 from itertools import product
 from .fermion_2d_tebd import insert,copy
-from .fermion_core import FermionTensor,tensor_contract
+from .fermion_core import FermionTensor
+
 ########### serial fxns #######################
-def apply_term(norm,term):
-    n = norm.num_tensors//2
+def apply_term(term,norm):
+    ftn = norm.copy()
+#    ftn = copy(norm)
+    n = ftn.num_tensors//2
     apply_order = list(term.ops.keys())
     apply_order.sort()
     for key in apply_order:
         where,op = term.ops[key]
-        ket = norm[norm.site_tag(*where),'KET']
+        ket = ftn[ftn.site_tag(*where),'KET']
         site_ix = ket.inds[-1]
         bnd = site_ix+'_'
         ket.reindex_({site_ix:bnd})
@@ -17,24 +22,18 @@ def apply_term(norm,term):
 
         TG = FermionTensor(op.copy(),inds=(site_ix,bnd),left_inds=(site_ix,),
                            tags=ket.tags)
-        norm = insert(norm,n,TG)
+        ftn = insert(ftn,n,TG)
         TG_tid,TG_site = TG.get_fermion_info()
-        norm.fermion_space.move(TG_tid,ket_site+1)
+        ftn.fermion_space.move(TG_tid,ket_site+1)
         
-#        print('############# before contract ###########')
-#        for tid,(tsr,site) in norm.fermion_space.tensor_order.items():
-#            print(site,tsr.inds,tsr.tags)
-        norm.contract_tags(ket.tags,which='all',inplace=True)
-#        print('############# after contract ###########')
-#        for tid,(tsr,site) in norm.fermion_space.tensor_order.items():
-#            print(site,tsr.inds,tsr.tags)
-#        exit()
-#    print('############# after contract ###########')
-#    for tid,(tsr,site) in norm.fermion_space.tensor_order.items():
-#        print(site,tsr.inds,tsr.tags,tsr.data.pattern)
-#        print(tsr.data)
-#    exit()
-    return norm,term.fac
+        ftn.contract_tags(ket.tags,which='all',inplace=True)
+    return ftn,term.fac
+def term_to_flat(term_map):
+    flat_map = dict()
+    for term_key,(plq_envs,fac) in term_map.items():
+        for (where,_),env in plq_envs.items():
+            flat_map[where,term_key] = env,fac
+    return flat_map
 def get_components_site(site,env,fac=1.0):
     scalar = env.contract()
     bra = env[site,'BRA']
@@ -42,14 +41,13 @@ def get_components_site(site,env,fac=1.0):
     env._pop_tensor(tid,remove_from_fermion_space='end')
     grad = env.contract(output_inds=bra.inds[::-1])
     return scalar*fac,grad*fac
-def parse_term_map(term_map):
+def flat_to_site(flat_map):
     site_map = dict()
-    for term_key,grad_site_map in term_map.items():
-        for where,grad_components in grad_site_map.items():
-            if where not in site_map:
-                site_map[where] = dict()
-            local_map = site_map[where]
-            local_map[term_key] = grad_components
+    for (where,term_key),val in flat_map.items():
+        if where not in site_map:
+            site_map[where] = dict()
+        local_map = site_map[where]
+        local_map[term_key] = val
     return site_map
 def get_local_grad(local_map):
     scalar_H = 0.0
@@ -67,10 +65,9 @@ def apply_terms(norm,H):
     ftn_map = dict()
     ftn_map['norm'] = norm,1.0
     for i,term in enumerate(H):
-        #ftn_map[i] = apply_term(copy(norm),term)
-        ftn_map[i] = apply_term(norm.copy(),term)
+        ftn_map[i] = apply_term(term,norm)
     return ftn_map
-def get_components_all(ftn_map,
+def get_plq_envs(ftn_map,
     max_bond=None,cutoff=1e-10,canonize=True,mode='mps',layer_tags=('KET','BRA'),
     **plq_env_opts):
     plq_env_opts['max_bond'] = max_bond
@@ -79,31 +76,21 @@ def get_components_all(ftn_map,
     plq_env_opts['mode'] = mode
     plq_env_opts['layer_tags'] = layer_tags
     x_bsz = y_bsz = 1
-    term_map = dict()
+    env_map = dict()
     for key,(ftn,fac) in ftn_map.items():
-#        print(key)
-        Ta,Tb = ftn['KET','I0,0'],ftn['KET','I1,0']
-        out = tensor_contract(Ta,Tb)
-#        print(out.inds)
-#        print(out.data)
-#        for tid,(tsr,site) in ftn.fermion_space.tensor_order.items():
-#            print(site,tsr.inds,tsr.tags,tsr.data.pattern)
-#            print(tsr.data)
-#        print(ftn)
         plq_envs = ftn.compute_plaquette_environments(x_bsz=x_bsz,y_bsz=y_bsz,
                    **plq_env_opts)
-#        print(key)
-        term_map[key] = get_components_term(plq_envs,ftn,fac)
-    return term_map
-def get_components_term(plq_envs,ftn,fac=1.0):
-    grad_site_map = dict() 
-    for (where,_),env in plq_envs.items():
-        site = ftn.site_tag(*where)
-        grad_site_map[where] = get_components_site(site,env,fac)
-    return grad_site_map
-def get_grad(site_map):
+        env_map[key] = plq_envs,fac
+    return env_map
+def get_components(flat_env_map,ftn):
+    component_map = dict()
+    for key,(env,fac) in flat_env_map.items():
+        site = ftn.site_tag(*key[0])
+        component_map[key] = get_components_site(site,env,fac)
+    return component_map
+def get_grad(component_map):
     grad_map = dict()
-    for where,local_map in site_map.items():
+    for where,local_map in component_map.items():
         grad_map[where] = get_local_grad(local_map)
     return grad_map
 class GlobalGrad():
@@ -121,8 +108,10 @@ class GlobalGrad():
             norm,_,self._bra = self._psi.make_norm(return_all=True,
                                         layer_tags=('KET','BRA'))
             ftn_map = apply_terms(norm,self.H)
-            component_map = get_components_all(ftn_map,max_bond=self.chi)
-            component_map = parse_term_map(component_map)
+            env_map = get_plq_envs(ftn_map,max_bond=self.chi)
+            flat_env_map = term_to_flat(env_map)
+            flat_component_map = get_components(flat_env_map,norm)
+            component_map = flat_to_site(flat_component_map)
             grad_map = get_grad(component_map)
             exit() 
 class Term():
@@ -135,11 +124,6 @@ def SpinlessFermion(t,v,Lx,Ly,symmetry='u1'):
     cre = creation(symmetry=symmetry)
     ann = cre.dagger
     pn = np.tensordot(cre,ann,axes=((1,),(0,)))
-#    print(cre)
-#    print(ann)
-#    print(pn)
-#    print(pn.dagger)
-#    exit()
     for i, j in product(range(Lx), range(Ly)):
         if i+1 != Lx:
             where = ((i,j),(i+1,j)) 
