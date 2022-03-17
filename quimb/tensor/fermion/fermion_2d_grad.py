0a8f7720890f9c1f804273195f42134b5831d30f
Ruojing Peng <rppeng@pauling.caltech.edu>
2022-03-16 17:54:24 -0700

    before implementing hessian method

--- quimb/tensor/fermion/fermion_2d_grad.py
+++ quimb/tensor/fermion/fermion_2d_grad.py
@@ -105,10 +105,10 @@ def worker_execution():
                                           *args, **kwargs)
         COMM.send(results, dest=0)
 ##################################################################
-def apply_term(term_key,H,psi,directory):
+def apply_term(term_key,H,psi_name,directory):
     sites,typ = term_key
     ops = H[term_key][0] 
-    psi = load_ftn_from_disc(psi)
+    psi = load_ftn_from_disc(psi_name)
     ftn = psi.make_norm(layer_tags=('KET','BRA'))
 
     N = ftn.num_tensors//2
@@ -241,7 +241,7 @@ def compute_site_components(site_tag,H,plq_envs):
         else:
             H0,H1 = H0+scal*H[key][-1],H1+data*H[key][-1]
     return site_tag,H1,N1,H0,N0
-def compute_grad(H,psi_fname,directory,layer_tags=('KET','BRA'),
+def compute_grad(H,psi_fname,directory,l=None,layer_tags=('KET','BRA'),
     max_bond=None,cutoff=1e-10,canonize=True,mode='mps'):
     compress_opts = dict()
     compress_opts['max_bond'] = max_bond
@@ -299,14 +299,17 @@ def compute_grad(H,psi_fname,directory,layer_tags=('KET','BRA'),
     args = [H,plq_envs]
     kwargs = dict()
     ls = parallelized_looped_function(fxn,iterate_over,args,kwargs)
-    grad = dict()
     site00 = norm.site_tag(0,0)
     for (site_tag,H1,N1,H0,N0) in ls:
-        E = H0/N0
+        if site_tag == site00:
+            H00,N00 = H0,N0
+    E = H00/N00
+    grad = dict()
+    for (site_tag,H1,N1,H0,N0) in ls:
         # f = <psi|H|psi>/<psi|psi>
         grad[site_tag] = (H1-N1*E)/N0
-        if site_tag == site00:
-            E00,N00 = E,N0
+        # f = <psi|H|psi>/<psi|psi>+lambda*(<psi|psi>-1)**2
+#        grad[site_tag] = (H1-N1*E)/N0+2.0*l*(N00-1.0)*N1
     # delete files 
     for _,fname in norm_benvs.items():
         delete_ftn_from_disc(fname) 
@@ -318,7 +321,7 @@ def compute_grad(H,psi_fname,directory,layer_tags=('KET','BRA'),
             delete_ftn_from_disc(fname)
     for _,fname in plq_envs.items():
         delete_ftn_from_disc(fname)
-    return grad,E00,N00
+    return grad,H00,N00,E
 def compute_norm(psi_name,directory,layer_tags=('KET','BRA'),
     max_bond=None,cutoff=1e-10,canonize=True,mode='mps'):
     compress_opts = dict()
@@ -449,62 +452,70 @@ class GlobalGrad():
                 ket.modify(data=cons.vector_to_tensor(x[start:stop],dq))
                 start = stop
         return psi
-    def compute_energy(self,x):
+    def compute_energy(self,x_):
+        if self.has_lambda:
+            x,l = x_[:len(x)-1],x_[-1]
+        else:
+            x,l = x_,None 
         psi = self.vec2fpeps(x) 
         write_ftn_to_disc(psi,self.tmp) 
         return compute_energy(self.H,self.tmp,self.directory,max_bond=self.chi)
-    def compute_grad(self,x):
+    def compute_grad(self,x_):
+        x = x_[:len(x_)-1] if self.has_lambda else x_
+        l = x_[-1] if self.has_lambda else None
         psi = self.vec2fpeps(x) 
         write_ftn_to_disc(psi,self.tmp) 
-        grad,E,N = compute_grad(self.H,self.tmp,self.directory,max_bond=self.chi)
-        self.k += 1
+        grad,H00,N00,E = compute_grad(self.H,self.tmp,self.directory,l=l,
+                                  max_bond=self.chi)
+
         g = []
         for i in range(psi.Lx):
             for j in range(psi.Ly):
                 site_tag = psi.site_tag(i,j)
                 cons = self.constructors[site_tag][0]
                 g.append(cons.tensor_to_vector(grad[site_tag]))
-        g = np.concatenate(g)        
+        g = np.concatenate(g)
+        gmax = np.amax(abs(g))
+        gl = (N00-1.0)**2 if self.has_lambda else None
+        if self.has_lambda:
+            g = np.concatenate([g,np.ones(1)*gl])
+        f = E+l*(N00-1.0)**2 if self.has_lambda else E
 
 #        E_ = compute_energy(self.H,self.tmp,self.directory,max_bond=self.chi)
 #        print(abs((E-E_)/E))
         if self.chi is None:
-            print('eval={},E={},N={},gmax={}'.format(
-                  self.k,E,N,np.amax(abs(g))))
+            print('eval={},E={},N={},gmax={},l={},gl={}'.format(
+                  self.k,E,N00,gmax,l,gl))
         else:
             E_ = compute_energy(self.H,self.tmp,self.directory,max_bond=self.chi//2)
-            print('eval={},E={},N={},Eerr={},gmax={}'.format(
-                  self.k,E,N,abs((E-E_)/E),np.amax(abs(g))))
+            print('eval={},E={},N={},Eerr={},gmax={},l={},gl={}'.format(
+                  self.k,E,N00,abs((E-E_)/E),gmax,l,gl))
+        self.k += 1
         if self.sep_energy_grad:
             return g
         else:
-            return E,g
+            return f,g
     def callback(self,x):
         psi = self.vec2fpeps(x)
         write_ftn_to_disc(psi,self.psi)
         delete_ftn_from_disc(self.tmp)
-        self.it += 1
         print('iter=',self.it)
+        self.it += 1
     def kernel(self,method='BFGS',options={'maxiter':100,'gtol':1e-5}):
         self.sep_energy_grad = False
-        if self.sep_energy_grad:
-            fun = self.compute_energy
-            jac = self.compute_grad
-        else:
-            fun = self.compute_grad 
-            jac = True
+        self.has_lambda = False
+        fun = self.compute_energy if self.sep_energy_grad else self.compute_grad
+        jac = self.compute_grad if self.sep_energy_grad else True
+        x0 = self.fpeps2vec(load_ftn_from_disc(self.psi))
+        if self.has_lambda:
+            x0 = np.concatenate([x0,1.0*np.ones(1)])
         self.it = 0
         self.k = 0
-        result = scipy.optimize.minimize(fun=fun,jac=jac,method=method,
-                 x0=self.fpeps2vec(load_ftn_from_disc(self.psi)),
+        result = scipy.optimize.minimize(fun=fun,jac=jac,method=method,x0=x0,
                  callback=self.callback,options=options)
-        psi = self.vec2fpeps(result.x)
+        x = result.x[:len(result.x)-1] if self.has_lambda else result.x
+        psi = self.vec2fpeps(x) 
         write_ftn_to_disc(psi,self.psi)
-        print(result.message)
-        print('total iter=',result.nit)
-        print('total eval=',self.k)
-        print('final energy=',result.fun)
-        print('gmax=',np.amax(abs(result.jac)))
         return result.fun
 ############## Hamiltonians ##############################
 def Hubbard(t,u,Lx,Ly,symmetry='u1',flat=True):
@@ -711,3 +722,329 @@ def custom(fun,x0,jac=None,callback=None,gtol=1e-5,maxiter=50,t0=0.5,**options):
         fold = f
         nit += 1
     return scipy.optimize.OptimizeResult(fun=f,jac=g,x=x,nit=nit,message='')
+def _minimize_bfgs(fun, x0, args=(), jac=None, callback=None, maxiter=None,
+                   gtol=1e-5, norm=np.Inf, eps=np.sqrt(np.finfo(float).eps), 
+                   disp=False, return_all=False, finite_diff_rel_step=None,
+                   hess=None,hessp=None,bounds=None,constraints=(),
+                   **unknown_options):
+    """
+    Minimization of scalar function of one or more variables using the
+    BFGS algorithm.
+    Options
+    -------
+    disp : bool
+        Set to True to print convergence messages.
+    maxiter : int
+        Maximum number of iterations to perform.
+    gtol : float
+        Gradient norm must be less than `gtol` before successful
+        termination.
+    norm : float
+        Order of norm (Inf is max, -Inf is min).
+    eps : float or ndarray
+        If `jac is None` the absolute step size used for numerical
+        approximation of the jacobian via forward differences.
+    return_all : bool, optional
+        Set to True to return a list of the best solution at each of the
+        iterations.
+    finite_diff_rel_step : None or array_like, optional
+        If `jac in ['2-point', '3-point', 'cs']` the relative step size to
+        use for numerical approximation of the jacobian. The absolute step
+        size is computed as ``h = rel_step * sign(x) * max(1, abs(x))``,
+        possibly adjusted to fit into the bounds. For ``method='3-point'``
+        the sign of `h` is ignored. If None (default) then step is selected
+        automatically.
+    """
+    from scipy.optimize import optimize
+    optimize._check_unknown_options(unknown_options)
+    retall = return_all
+
+    x0 = np.asarray(x0).flatten()
+    if x0.ndim == 0:
+        x0.shape = (1,)
+    if maxiter is None:
+        maxiter = len(x0) * 200
+
+    sf = optimize._prepare_scalar_function(fun, x0, jac, args=args, epsilon=eps,
+                                  finite_diff_rel_step=finite_diff_rel_step)
+
+    f = sf.fun
+    myfprime = sf.grad
+
+    old_fval = f(x0)
+    gfk = myfprime(x0)
+
+    k = 0
+    N = len(x0)
+    I = np.eye(N, dtype=int)
+    Hk = I
+
+    # Sets the initial step guess to dx ~ 1
+    old_old_fval = old_fval + np.linalg.norm(gfk) / 2
+
+    xk = x0
+    if retall:
+        allvecs = [x0]
+    warnflag = 0
+    gnorm = optimize.vecnorm(gfk, ord=norm)
+    while (gnorm > gtol) and (k < maxiter):
+        pk = -np.dot(Hk, gfk)
+        try:
+            alpha_k, fc, gc, old_fval, old_old_fval, gfkp1 = \
+                     optimize._line_search_wolfe12(f, myfprime, xk, pk, gfk,
+                         old_fval, old_old_fval, amin=1e-100, amax=1e100)
+        except optimize._LineSearchError:
+            # Line search failed to find a better solution.
+            warnflag = 2
+            break
+
+        xkp1 = xk + alpha_k * pk
+        if retall:
+            allvecs.append(xkp1)
+        sk = xkp1 - xk
+        nsk = np.linalg.norm(sk)
+        sk /= nsk
+        xk = xkp1
+        if gfkp1 is None:
+            gfkp1 = myfprime(xkp1)
+
+        yk = gfkp1 - gfk
+        yk /= nsk
+        gfk = gfkp1
+        if callback is not None:
+            callback(xk)
+        k += 1
+        gnorm = optimize.vecnorm(gfk, ord=norm)
+        if (gnorm <= gtol):
+            break
+
+        if not np.isfinite(old_fval):
+            # We correctly found +-Inf as optimal value, or something went
+            # wrong.
+            warnflag = 2
+            break
+
+        rhok_inv = np.dot(yk, sk)
+        print('rhok_inv={},nsk={},nyk={},alpha={}'.format(rhok_inv,nsk,np.linalg.norm(yk),alpha_k))
+        # this was handled in numeric, let it remaines for more safety
+        if rhok_inv == 0.:
+            rhok = 1000.0
+            if disp:
+                print("Divide-by-zero encountered: rhok assumed large")
+        else:
+            rhok = 1. / rhok_inv
+
+        A1 = I - sk[:, np.newaxis] * yk[np.newaxis, :] * rhok
+        A2 = I - yk[:, np.newaxis] * sk[np.newaxis, :] * rhok
+        Hk = np.dot(A1, np.dot(Hk, A2)) + (rhok * sk[:, np.newaxis] *
+                                                 sk[np.newaxis, :])
+
+    fval = old_fval
+
+    if warnflag == 2:
+        msg = optimize._status_message['pr_loss']
+    elif k >= maxiter:
+        warnflag = 1
+        msg = optimize._status_message['maxiter']
+    elif np.isnan(gnorm) or np.isnan(fval) or np.isnan(xk).any():
+        warnflag = 3
+        msg = optimize._status_message['nan']
+    else:
+        msg = optimize._status_message['success']
+
+    if disp:
+        print("%s%s" % ("Warning: " if warnflag != 0 else "", msg))
+        print("         Current function value: %f" % fval)
+        print("         Iterations: %d" % k)
+        print("         Function evaluations: %d" % sf.nfev)
+        print("         Gradient evaluations: %d" % sf.ngev)
+
+    result = optimize.OptimizeResult(fun=fval, jac=gfk, hess_inv=Hk, nfev=sf.nfev,
+                            njev=sf.ngev, status=warnflag,
+                            success=(warnflag == 0), message=msg, x=xk,
+                            nit=k)
+    if retall:
+        result['allvecs'] = allvecs
+    return result
+def _minimize_newtoncg(fun, x0, args=(), jac=None, hess=None, hessp=None,
+                       callback=None, xtol=1e-5, eps=np.sqrt(np.finfo(float).eps), 
+                       maxiter=None, disp=False, return_all=False,
+                       bounds=None,constraints=(),
+                       **unknown_options):
+    """
+    Minimization of scalar function of one or more variables using the
+    Newton-CG algorithm.
+    Note that the `jac` parameter (Jacobian) is required.
+    Options
+    -------
+    disp : bool
+        Set to True to print convergence messages.
+    xtol : float
+        Average relative error in solution `xopt` acceptable for
+        convergence.
+    maxiter : int
+        Maximum number of iterations to perform.
+    eps : float or ndarray
+        If `hessp` is approximated, use this value for the step size.
+    return_all : bool, optional
+        Set to True to return a list of the best solution at each of the
+        iterations.
+    """
+    from scipy.optimize import optimize
+    optimize._check_unknown_options(unknown_options)
+    if jac is None:
+        raise ValueError('Jacobian is required for Newton-CG method')
+    fhess_p = hessp
+    fhess = hess
+    avextol = xtol
+    epsilon = eps
+    retall = return_all
+
+    x0 = np.asarray(x0).flatten()
+    # TODO: add hessp (callable or FD) to ScalarFunction?
+    sf = optimize._prepare_scalar_function(
+        fun, x0, jac, args=args, epsilon=eps, hess=hess
+    )
+    f = sf.fun
+    fprime = sf.grad
+    _h = sf.hess(x0)
+
+    # Logic for hess/hessp
+    # - If a callable(hess) is provided, then use that
+    # - If hess is a FD_METHOD, or the output fom hess(x) is a LinearOperator
+    #   then create a hessp function using those.
+    # - If hess is None but you have callable(hessp) then use the hessp.
+    # - If hess and hessp are None then approximate hessp using the grad/jac.
+
+    if (hess in optimize.FD_METHODS or isinstance(_h, scipy.sparse.linalg.LinearOperator)):
+        fhess = None
+
+        def _hessp(x, p, *args):
+            return sf.hess(x).dot(p)
+
+        fhess_p = _hessp
+
+    def terminate(warnflag, msg):
+        if disp:
+            print(msg)
+            print("         Current function value: %f" % old_fval)
+            print("         Iterations: %d" % k)
+            print("         Function evaluations: %d" % sf.nfev)
+            print("         Gradient evaluations: %d" % sf.ngev)
+            print("         Hessian evaluations: %d" % hcalls)
+        fval = old_fval
+        result = optimize.OptimizeResult(fun=fval, jac=gfk, nfev=sf.nfev,
+                                njev=sf.ngev, nhev=hcalls, status=warnflag,
+                                success=(warnflag == 0), message=msg, x=xk,
+                                nit=k)
+        if retall:
+            result['allvecs'] = allvecs
+        return result
+
+    hcalls = 0
+    if maxiter is None:
+        maxiter = len(x0)*200
+    cg_maxiter = 20*len(x0)
+
+    xtol = len(x0) * avextol
+    update = [2 * xtol]
+    xk = x0
+    if retall:
+        allvecs = [xk]
+    k = 0
+    gfk = None
+    old_fval = f(x0)
+    old_old_fval = None
+    float64eps = np.finfo(np.float64).eps
+#    while np.add.reduce(np.abs(update)) > xtol:
+    b = np.ones(len(x0))
+    while np.amax(np.abs(b)) > avextol:
+        if k >= maxiter:
+            msg = "Warning: " + _status_message['maxiter']
+            return terminate(1, msg)
+        # Compute a search direction pk by applying the CG method to
+        #  del2 f(xk) p = - grad f(xk) starting from 0.
+        b = -fprime(xk)
+        maggrad = np.add.reduce(np.abs(b))
+        eta = np.min([0.5, np.sqrt(maggrad)])
+        termcond = eta * maggrad
+        xsupi = np.zeros(len(x0), dtype=x0.dtype)
+        ri = -b
+        psupi = -ri
+        i = 0
+        dri0 = np.dot(ri, ri)
+
+        if fhess is not None:             # you want to compute hessian once.
+            A = sf.hess(xk)
+            hcalls = hcalls + 1
+
+        for k2 in range(cg_maxiter):
+            if np.add.reduce(np.abs(ri)) <= termcond:
+                break
+            if np.amax(abs(ri)) < avextol:
+                break
+            if fhess is None:
+                if fhess_p is None:
+                    Ap = optimize.approx_fhess_p(xk, psupi, fprime, epsilon)
+                else:
+                    Ap = fhess_p(xk, psupi, *args)
+                    hcalls = hcalls + 1
+            else:
+                if isinstance(A, HessianUpdateStrategy):
+                    # if hess was supplied as a HessianUpdateStrategy
+                    Ap = A.dot(psupi)
+                else:
+                    Ap = np.dot(A, psupi)
+            # check curvature
+            Ap = np.asarray(Ap).squeeze()  # get rid of matrices...
+            curv = np.dot(psupi, Ap)
+            print('curv={},npi={},rmax={}'.format(curv/np.dot(psupi,psupi),np.linalg.norm(psupi),np.amax(abs(ri))))
+            if 0 <= curv <= 3 * float64eps:
+                break
+            elif curv < 0:
+                if (i > 0):
+                    break
+                else:
+                    # fall back to steepest descent direction
+                    xsupi = dri0 / (-curv) * b
+                    break
+            alphai = dri0 / curv
+            xsupi = xsupi + alphai * psupi
+            ri = ri + alphai * Ap
+            dri1 = np.dot(ri, ri)
+            betai = dri1 / dri0
+            psupi = -ri + betai * psupi
+            i = i + 1
+            dri0 = dri1          # update np.dot(ri,ri) for next time.
+        else:
+            # curvature keeps increasing, bail out
+            msg = ("Warning: CG iterations didn't converge. The Hessian is not "
+                   "positive definite.")
+            return terminate(3, msg)
+
+        pk = xsupi  # search direction is solution to system.
+        gfk = -b    # gradient at xk
+
+        try:
+            alphak, fc, gc, old_fval, old_old_fval, gfkp1 = \
+                     optimize._line_search_wolfe12(f, fprime, xk, pk, gfk,
+                                          old_fval, old_old_fval)
+        except optimize._LineSearchError:
+            # Line search failed to find a better solution.
+            msg = "Warning: " + _status_message['pr_loss']
+            return terminate(2, msg)
+
+        update = alphak * pk
+        xk = xk + update        # upcast if necessary
+        if callback is not None:
+            callback(xk)
+        if retall:
+            allvecs.append(xk)
+        k += 1
+    else:
+        if np.isnan(old_fval) or np.isnan(update).any():
+            return terminate(3, _status_message['nan'])
+
+        msg = optimize._status_message['success']
+        return terminate(0, msg)
+
