12b5f146cc7b805bbd6fb02c819ceaef0e27ad65
Ruojing Peng <rppeng@pauling.caltech.edu>
2023-12-22 11:07:44 -0800

    push

--- quimb/tensor/product_2d_vmc.py
+++ quimb/tensor/product_2d_vmc.py
@@ -134,94 +134,76 @@ class CNN2D(CNN,AmplitudeFactory2D):
         super().__init__(Lx*Ly,D,**kwargs)
     def init_dim(self):
         self.lx,self.ly = self.Lx,self.Ly
-#class CNN2D1(CNN2D):
-#    def break1(self,key):
-#        if not (self.lx==1 and self.ly==1):
-#            return False
-#        self.sh[key] = self.sh[key][:-1] + (nf,)
-#        return True
-#    def set_layer(self,i,Dix,D):
-#        D1 = D[Dix] if Dix<len(D) else D[-1]
-#        D2 = D[Dix+1] if Dix+1<len(D) else D[-1]
-#        key = i,1
-#        self.param_keys.append(key)
-#        self.sh[key] = D1,D1,D2
-#        if self.lx==1:
-#            self.ly -= 1
-#            return i+1,Dix+1,key
-#        if self.ly==1:
-#            self.lx -= 1
-#            return i+1,Dix+1,key
-#        D3 = D[Dix+2] if Dix+2<len(D) else D[-1]
-#        D4 = D[Dix+3] if Dix+3<len(D) else D[-1]
-#        key = i,2
-#        self.param_keys.append(key)
-#        self.sh[key] = D2,D2,D3
-#        key = i,3
-#        self.param_keys.append(key)
-#        self.sh[key] = D3,D3,D4
-#        self.lx -= 1
-#        self.ly -= 1
-#        return i+1,Dix+3,key
-#    def break2(self,v):
-#        if not (self.lx==1 and self.ly==1):
-#            return False,None
-#        if isinstance(v,dict):
-#            v = list(v.values())
-#        return True,v[0]
-#    def layer_forward(self,l,v):
-#        if self.lx>1 and self.ly>1:
-#            return self.layer_forward2D(l,v)
-#        else:
-#            return self.layer_forward1D(l,v)
-#    def layer_forward2D(self,l,v):
-#        b = dict()
-#        T1 = self.params[l,1]
-#        for i,j in itertools.product(range(self.lx),range(self.ly)):
-#            if j+1<self.ly:
-#                b[(i,j),(i,j+1)] = self.jnp.einsum('ijk,i,j->k',T1,v[i,j],v[i,j+1])
-#            if i+1<self.lx:
-#                b[(i,j),(i+1,j)] = self.jnp.einsum('ijk,i,j->k',T1,v[i,j],v[i+1,j])
-#
-#        v = dict()
-#        T2 = self.params[l,2]
-#        T3 = self.params[l,3]
-#        for i,j in itertools.product(range(self.lx-1),range(self.ly-1)):
-#            v1 = self.jnp.einsum('ijk,i,j->k',T2,b[(i,j),(i,j+1)],b[(i+1,j),(i+1,j+1)])
-#            v2 = self.jnp.einsum('ijk,i,j->k',T2,b[(i,j),(i+1,j)],b[(i,j+1),(i+1,j+1)])
-#            v[i,j] = self.jnp.einsum('ijk,i,j->k',T3,v1,v2)
-#        self.lx -= 1
-#        self.ly -= 1
-#        return l+1,v
-#    def layer_forward1D(self,l,v):
-#        if isinstance(v,dict):
-#            if self.lx == 1:
-#                v = [v[0,j] for j in range(self.ly)] 
-#                l = self.ly
-#            if self.ly == 1:
-#                v = [v[i,0] for i in range(self.lx)] 
-#                l = self.lx
-#        T = self.params[l,1]
-#        vnew = [None] * (l-1) 
-#        for i in range(l-1):
-#            vnew[i] = self.jnp.einsum('ijk,i,j->k',T,v[i],v[i+1]) 
-#        if self.lx==1:
-#            self.ly -= 1
-#        if self.ly==1:
-#            self.lx -= 1
-#        return l+1,vnew
-class CNN2D1(CNN2D,AmplitudeFactory2D):
+class CNN2D1(CNN2D):
+    def set_layer(self,l,Dix,D):
+        D1 = D[Dix] if Dix<len(D) else D[-1]
+        D2 = D[Dix+1] if Dix+1<len(D) else D[-1]
+        if self.ly > 1:
+            key = l,'h'
+            self.param_keys.append(key)
+            self.sh[key] = self.lx,self.ly-1,D1,D1,D2
+        if self.lx > 1:
+            key = l,'v'
+            self.param_keys.append(key)
+            self.sh[key] = self.lx-1,self.ly,D1,D1,D2
+
+        if self.lx > 1:
+            self.lx -= 1
+        if self.ly > 1:
+            self.ly -= 1
+        _break = False
+        if self.lx==1 and self.ly==1:
+            _break = True
+            for key in [(l,'h'),(l,'v')]:
+                if key in self.sh:
+                    self.sh[key] = self.sh[key][:-1] + (self.nf,)  
+
+        return l+1,Dix+1,_break
+    def layer_forward(self,l,v):
+        vnew = dict()
+        if self.lx == 1:
+            i = 0
+            T = self.params[l,'h']
+            for j in range(self.ly-1):
+                vnew[i,j] = self.jnp.einsum('i,j,ijk->k',v[i,j],v[i,j+1],T[i,j,...])
+            self.ly -= 1
+            if self.ly == 1:
+                return None,vnew[0,0],True
+            return l+1,vnew,False
+        if self.ly == 1:
+            j = 0
+            T = self.params[l,'v']
+            for i in range(self.lx-1):
+                vnew[i,j] = self.jnp.einsum('i,j,ijk->k',v[i,j],v[i+1,j],T[i,j,...])
+            self.lx -= 1
+            if self.lx == 1:
+                return None,vnew[0,0],True
+            return l+1,vnew,False
+
+        b = dict()
+        Th = self.params[l,'h']
+        Tv = self.params[l,'v']
+        for i,j in itertools.product(range(self.lx),range(self.ly)):
+            if j+1<self.ly:
+                b[(i,j),(i,j+1)] = self.jnp.einsum('i,j,ijk->k',v[i,j],v[i,j+1],Th[i,j,...])
+            if i+1<self.lx:
+                b[(i,j),(i+1,j)] = self.jnp.einsum('i,j,ijk->k',v[i,j],v[i+1,j],Tv[i,j,...])
+
+        for i,j in itertools.product(range(self.lx-1),range(self.ly-1)):
+            vnew[i,j] = b[(i,j),(i,j+1)]*b[(i+1,j),(i+1,j+1)] + b[(i,j),(i+1,j)]*b[(i,j+1),(i+1,j+1)]
+        self.lx -= 1
+        self.ly -= 1
+        if self.lx == 1 and self.ly == 1:
+            return None,vnew[0,0],True
+        return l+1,vnew,False 
+class CNN2D2(CNN2D,AmplitudeFactory2D):
     def set_layer(self,l,Dix,D):
         D1 = D[Dix] if Dix<len(D) else D[-1]
         D2 = D[Dix+1] if Dix+1<len(D) else D[-1]
         key = l,1
         self.param_keys.append(key) 
         self.sh[key] = max(self.lx-1,1),max(self.ly-1,1),D1,D1,D2
-        if self.lx==1:
-            self.ly -= 1
-        elif self.ly==1:
-            self.lx -= 1
-        else:
+        if self.lx > 1 and self.ly > 1:
             key = l,2
             self.param_keys.append(key) 
             self.sh[key] = self.lx-1,self.ly-1,D1,D2,D2
@@ -229,49 +211,24 @@ class CNN2D1(CNN2D,AmplitudeFactory2D):
             key = l,3
             self.param_keys.append(key) 
             self.sh[key] = self.lx-1,self.ly-1,D1,D2,D2
-            self.lx -= 1 
+
+        if self.lx > 1:
+            self.lx -= 1
+        if self.ly > 1:
             self.ly -= 1
-        
+
         _break = False
         if self.lx==1 and self.ly==1:
             _break = True
             self.sh[key] = self.sh[key][:-1] + (self.nf,)  
         return l+1,Dix+1,_break
-    def input_layer(self,config):
-        v = dict()
-        l = 0
-        if self.lx == 1:
-            i = 0
-            for j in range(self.ly-1):
-                sites = (i,j),(i,j+1)
-                c = [config[self.flatten(site)] for site in sites]
-                v[i,j] = self.params[l,1][i,j,c[0],c[1],:]
-            self.ly -= 1
-            return v
-        if self.ly == 1:
-            j = 0
-            for i in range(self.lx-1):
-                sites = (i,j),(i+1,j)
-                c = [config[self.flatten(site)] for site in sites]
-                v[i,j] = self.params[l,1][i,j,c[0],c[1],:]
-            self.lx -= 1
-            return v
-        for i,j in itertools.product(range(self.lx-1),range(self.ly-1)):
-            sites = (i,j),(i,j+1),(i+1,j+1),(i+1,j)
-            c = [config[self.flatten(site)] for site in sites]
-            T1 = self.params[l,1][i,j,c[0],c[1],:]
-            T2 = self.params[l,2][i,j,c[2],:,:] 
-            T3 = self.params[l,3][i,j,c[3],:,:] 
-            v[i,j] = self.jnp.matmul(self.jnp.matmul(T1,T2),T3)
-        self.lx -= 1
-        self.ly -= 1
-        return v 
     def layer_forward(self,l,v):
         vnew = dict()
+        T1 = self.params[l,1]
         if self.lx == 1:
             i = 0
             for j in range(self.ly-1):
-                vnew[i,j] = self.jnp.einsum('i,j,ijk->k',v[i,j],v[i,j+1],self.params[l,1][i,j,...])
+                vnew[i,j] = self.jnp.einsum('i,j,ijk->k',v[i,j],v[i,j+1],T1[i,j,...])
             self.ly -= 1
             if self.ly == 1:
                 return None,vnew[0,0],True
@@ -279,16 +236,17 @@ class CNN2D1(CNN2D,AmplitudeFactory2D):
         if self.ly == 1:
             j = 0
             for i in range(self.lx-1):
-                vnew[i,j] = self.jnp.einsum('i,j,ijk->k',v[i,j],v[i+1,j],self.params[l,1][i,j,...])
+                vnew[i,j] = self.jnp.einsum('i,j,ijk->k',v[i,j],v[i+1,j],T1[i,j,...])
             self.lx -= 1
             if self.lx == 1:
                 return None,vnew[0,0],True
             return l+1,vnew,False
+        T2 = self.params[l,2]
+        T3 = self.params[l,3]
         for i,j in itertools.product(range(self.lx-1),range(self.ly-1)):
-            sites = (i,j),(i,j+1),(i+1,j+1),(i+1,j)
-            vij = self.jnp.einsum('i,j,ijk->k',v[i,j],v[i,j+1],self.params[l,1][i,j,...])
-            vij = self.jnp.einsum('i,j,ijk->k',v[i+1,j+1],vij,self.params[l,2][i,j,...]) 
-            vnew[i,j] = self.jnp.einsum('i,j,ijk->k',v[i+1,j],vij,self.params[l,3][i,j,...])
+            vij = self.jnp.einsum('i,j,ijk->k',v[i,j],v[i,j+1],T1[i,j,...])
+            vij = self.jnp.einsum('i,j,ijk->k',v[i+1,j+1],vij,T2[i,j,...]) 
+            vnew[i,j] = self.jnp.einsum('i,j,ijk->k',v[i+1,j],vij,T3[i,j,...])
         self.lx -= 1
         self.ly -= 1
         if self.lx == 1 and self.ly == 1:
